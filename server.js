4627db4939ac58d33f22c6c4b5796c282b1ca4c9
Anna Peterson <reveragb@gmail.com>
2023-03-03 11:22:22 -0700

    Add changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const data = require('./data');
+const cors = require('cors')
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001'
+}))
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'The Book of Many Things';
@@ -11,126 +14,17 @@ app.get('/', (request, response) => {
   response.send('I cast Fireball');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
-});
-
-app.locals.data = [
-  {
-  rules: 
-    [
-      "Character Creation: To play D&D, you will need to create a character. This involves choosing a race, class, and background for your character, as well as assigning values to six ability scores (strength, dexterity, constitution, intelligence, wisdom, and charisma). Your character\'s abilities and skills will be determined by these scores.", 
-
-      "Ability Checks: During the game, you will need to roll a 20-sided die (also known as a d20) to determine the success of your character\'s actions. The DM will assign a target number (known as a Difficulty Class or DC) based on the difficulty of the action, and you will need to roll equal to or higher than that number to succeed.", 
-
-      "Combat: When your character is in combat, you will need to roll for initiative to determine when your turn comes up. During your turn, you can take various actions, such as attacking an enemy, casting a spell, or using an item. You will need to roll a d20 to determine if your attack hits, and if it does, you will roll damage based on your weapon or spell.", 
-
-      "Spellcasting: If your character is a spellcaster, you will have access to a variety of spells with different effects. To cast a spell, you will need to use up one of your spell slots (which replenish after a rest), and then make a spell attack roll or have the target make a saving throw against your spell's DC.", 
-
-      "Experience Points: As you play the game and complete adventures, your character will earn experience points (XP). Once you earn enough XP, your character will level up, which allows you to improve your abilities, gain new spells or abilities, and become more powerful overall.", 
-
-      "These are just the basics of D&D, but they should give you a good starting point for understanding the game. The most important thing is to have fun and be creative!"
-    ],
-
-  races: 
-    [
-      "Dungeons & Dragons (D&D) races are essentially the different humanoid or semi-humanoid species that a player can choose to play as in the game. Each race has unique characteristics, such as physical traits, cultural background, and special abilities.", 
-
-      "Some common races in D&D include humans, elves, dwarves, halflings, and orcs. Each race has its own strengths and weaknesses, and some may be better suited for certain classes or roles within the game. The choice of race can also affect how the character is perceived and treated by non-player characters within the game world."
-    ],
-
-  classes: 
-    [
-      "Dungeons & Dragons (D&D) classes refer to the different roles or archetypes that a player can choose to take on within the game. Here's a brief overview of each class:", 
-
-      "Barbarian: A fierce warrior who relies on brute strength and primal rage to overpower enemies. AKA 'I like to bonk people with things. Many times. In some cases, with other people.'", 
-
-      "Bard: A versatile performer who uses magic and music to inspire allies, control enemies, and heal wounds. AKA 'I can bring you back to life through the power of song, or I can insult you to death.'", 
-
-      "Cleric: A divine spellcaster who draws power from a deity to heal allies, harm enemies, and support the party in a variety of ways. AKA 'a priest that can literally backhand you with the power of God. And Pray the life back into you.'", 
-
-      "Druid: A nature-based spellcaster who can shapeshift into animals, call upon natural forces, and manipulate the environment. AKA 'frail hippie who can turn into a velociraptor and drop a volcano on you.'", 
-
-      "Fighter: A skilled warrior who excels in combat, with a wide range of abilities and weapon proficiencies. AKA 'Let me bonk you several times in different ways.'", 
-
-      "Monk: A disciplined martial artist who can harness their own ki energy to perform incredible feats of strength, agility, and speed. AKA 'Only weaklings use armor and weapons. I will punch you many times with my fists, and I can literally catch bullets. I'm Bruce Lee, suckas!'", 
-
-      "Paladin: A holy warrior who channels divine power to protect and support the party, while smiting foes with righteous fury. AKA 'Fighter + Cleric = person who pinky-promised a god to spread the good word through violence. Surprisingly good at the art of conversation. Oh, and they can heal you with a head pat.'", 
-
-      "Ranger: A skilled tracker and survivalist who can navigate through any environment, hunt prey, and cast spells to aid allies or hinder enemies. AKA 'I want to be a fighter, but druid sounds cool too. Let me bonk some stuff, talk to this tree, and blend into the foliage while my pet bear mauls you to death.'", 
-
-      "Rogue: A stealthy thief or assassin who excels at sneaking, picking locks, disarming traps, and dealing massive damage with surprise attacks. AKA 'I am a sneaky sneaky murder hobo with a canonically tragic backstory.'", 
-
-      "Sorcerer: A spellcaster who has innate magical abilities, drawing upon a limited number of spells known but with great flexibility in casting. AKA 'my body is so frail a high five could knock me over, but my dad is a god so now I'm a magic prodigy. No studying for me (lol wizards)!'", 
-
-      "Warlock: A spellcaster who has made a pact with a powerful entity (such as a demon or a deity) to gain magical abilities and otherworldly powers. AKA 'I signed a shady contract with a god in a dark alleyway in order to do magic. But hey, at least they have dental insurance and other cool perks.'", 
-
-      "Wizard: A studious spellcaster who has mastered a vast array of spells and can cast them using a spellbook. AKA 'I may be the nerd of nerds with the physical strength of tissue paper, but I can cripple armies with one strategically-placed fireball or turn a very dangerous monster into a turtle.'"
-    ],
-
-  backgrounds: 
-    [
-      "In Dungeons & Dragons, a character's background provides a narrative framework for their experiences and skills prior to becoming an adventurer. Each background comes with a set of proficiencies, equipment, and roleplaying prompts that can help players flesh out their characters' personalities and backstories. Examples of backgrounds include noble, criminal, folk hero, sage, and acolyte, among others. Choosing a background is an important part of character creation, as it can inform the player's decisions and actions throughout the game."
-    ],
-
-  ability_scores: 
-    [
-      "In Dungeons & Dragons, a character's ability scores represent their innate capabilities in six different areas: Strength, Dexterity, Constitution, Intelligence, Wisdom, and Charisma. These scores are typically determined by rolling dice or using a point-buy system during character creation. Each ability score has a numerical value that can range from 1 to 20 (or higher, in some cases). These scores are used to determine the character's modifiers, which are added to dice rolls when attempting tasks related to that ability. For example, a character with a high Strength score will have a bonus to their attack rolls and damage with melee weapons, while a character with a high Charisma score will be more persuasive and able to influence others. Understanding ability scores is important for creating a character that fits the player's desired playstyle and role within the group."
-    ],
-
-  skills: 
-    [
-      "In Dungeons & Dragons, skills represent a character's proficiency in certain areas of expertise, such as Athletics, Stealth, or Persuasion. Each skill is associated with an ability score, such as Strength, Dexterity, or Charisma, and the character's proficiency bonus is added to any ability check that involves that skill. For example, a character with proficiency in the Perception skill would add their proficiency bonus to any Wisdom (Perception) checks they make to notice something in their environment. Characters can gain proficiency in skills through their class, race, or background, and can also acquire new skills through training or experience. Understanding skills is important for developing a character's unique strengths and abilities, and for determining how they can contribute to the group's success."
-    ],
-
-  proficiencies: 
-    [
-      "In Dungeons & Dragons, proficiencies represent a character's training and expertise in certain areas, such as weapons, armor, tools, languages, and skills. Proficiencies can be gained through a character's class, race, or background, and they provide bonuses to the character's ability checks, attack rolls, and other actions related to that proficiency. For example, a character proficient in the Stealth skill would add their proficiency bonus to any Dexterity (Stealth) checks they make to avoid being seen or heard. Proficiencies are an important part of character creation and development, as they help define a character's strengths and abilities. Choosing which proficiencies to focus on can also help players create a character that fits their desired playstyle and role within the group."
-    ],
-
-  alignments: 
-    [
-      "In Dungeons & Dragons, alignments represent a character's moral and ethical outlook on the world. There are nine different alignments, each representing a combination of two different moral axes: Good-Neutral-Evil and Lawful-Neutral-Chaotic.",
-
-      "Good characters are typically concerned with the welfare of others, while Evil characters prioritize their own interests over others. Neutral characters fall somewhere in between.",
-
-      "Lawful characters value order and structure, while Chaotic characters prioritize individual freedom and spontaneity. Neutral characters again fall somewhere in between.",
-
-      "A character's alignment can influence their actions and interactions with others, as well as how the game's NPCs perceive and react to them. However, it's important to note that alignment is not meant to be a strict set of rules that govern a character's behavior, and players should feel free to interpret and play their characters in a way that feels authentic and fun to them."
-    ],
-
-  dice: 
-    {
-      general: 
-        [
-          "The number above refers to the number of sides on each die, preceded by a 'd'. So when the game master asks you to roll a 'd6', you'll be using the six-sided die. The die you'll roll the most in D&D is the 20-sided die or d20. Whenever you attempt an action, you'll use the d20, plus your character's skills, to do so.",
-
-          "The other dice in the set will determine different aspects of D&D, such as how much damage your sword deals to an enemy. During the game, you may be asked to roll multiple dice of the same type. The common phrasing will be for the amount of dice at the beginning, followed by the type of dice. For example, when asked to roll 6 of your eight-sided dice, you will see this written as 8d6.",
-
-          "Some might ask, 'how many dice do you need to play D&D?' The answer is simple: there is no limit. Play with one 7-dice D&D set, or embrace your inner dice goblin and have a gigantic bag of assorted dice to choose from! No shame."
-        ],
-
-      d20: "The D20 is used any time you want to know if an attempt is successful: attacks, saving throws, skill checks, and ability checks.",
-
-      d12: "The twelve sided die is often used for damage for large weapons carried by a raging barbarian. Each face is shaped like a pentagon. These dice also stack very well for those long dice-stacking intervals between combat turns.",
-
-      d10: "The ten sided die is used often while playing D&D. There are actually two ten sided dice in a standard 7-dice D&D set. The faces of one are numbered 0 to 9, and the faces of the other go from 00 to 90 by tens. To make a 'percentile' roll, you roll both die and add them together, with one exception: a double zero roll is 100.",
-
-      d8: "The eight sided die, each face is triangular and it resembles two pyramids attached base-to-base. Often used for damage done by larger weapons." ,
-
-      d6: "The standard cube shape die found in most boardgames is also used in D&D. Many gamers gather a set of D6 for quickly rolling multiples, like the original 3D6 rolls for attributes during character creation. This practice later turned into rolling 4D6, discounting the lowest roll, and assigning each number where the player wants.", 
-
-      d4: "The unloved D4, also known as the caltrop. If you have stepped on a D4 with bare feet, you will never forget it. Because of its large flat sides it does not roll as well as the other shapes. In Dungeons & Dragons, the D4 is used where you want low variation, and an average roll of 2.5. Damage rolls for small weapons is a common use, as well as use for healing spells."
-    }
-  }
-];
-
 app.get('/api/v1/data', (request, response) => {
-  const data = app.locals.data;
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  response.setHeader('Access-Control-Allow-Headers', '*');
 
   if (!data) {
     return response.status(404);
   }
 
   response.status(200).json(data);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });

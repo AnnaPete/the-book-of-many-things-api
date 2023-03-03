143fdf32ab59ac7c50451fb20a18b077cc8a1d06
Anna Peterson <reveragb@gmail.com>
2023-03-03 11:26:09 -0700

    Update cors

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const data = require('./data');
-const cors = require('cors')
 
-app.use(cors({
-  origin: 'http://localhost:3001'
-}))
+app.use(cors());
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'The Book of Many Things';
@@ -15,9 +13,6 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/v1/data', (request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-
   if (!data) {
     return response.status(404);
   }
